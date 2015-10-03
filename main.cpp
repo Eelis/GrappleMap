cf0df46dd722d617b9b3c5454c74251c620f192c
Eelis van der Weegen <eelis@eelis.net>
2015-10-03 13:12:17 +0200

    Put viables stuff in separate file.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "persistence.hpp"
 #include "math.hpp"
 #include "positions.hpp"
+#include "viables.hpp"
 #include <GLFW/glfw3.h>
 #include <array>
 #include <cmath>
@@ -44,7 +45,7 @@ void pillar(V3 from, V3 to, double from_radius, double to_radius, unsigned faces
 	for (unsigned i = 0; i != faces; ++i)
 	{
 		glNormal(a * sin(i * s) + b * cos(i * s));
-			glVertex(from + a * from_radius * std::sin( i    * s) + b * from_radius * std::cos( i    * s));
+		glVertex(from + a * from_radius * std::sin( i    * s) + b * from_radius * std::cos( i    * s));
 		glVertex(to   + a *   to_radius * std::sin( i    * s) + b *   to_radius * std::cos( i    * s));
 
 		glNormal(a * sin((i + 1) * s) + b * cos((i + 1) * s));
@@ -90,168 +91,6 @@ struct NextPos
 	unsigned sequence;
 	unsigned position;
 };
-
-struct Viable
-{
-	double dist;
-	unsigned begin, end; // half-open range, never empty
-	V3 beginV3, endV3;
-	V2 beginxy, endxy;
-};
-
-struct ViablesForJoint
-{
-	double total_dist;
-	std::map<unsigned /* sequence */, Viable> viables;
-};
-
-Viable viableFront(Sequence const & sequence, PlayerJoint j, Camera const & camera)
-{
-	auto xyz = sequence.positions.front()[j];
-	auto xy = world2xy(camera, xyz);
-	return Viable{0, 0, 1, xyz, xyz, xy, xy};
-}
-
-Viable viableBack(Sequence const & seq, PlayerJoint j, Camera const & camera)
-{
-	auto xyz = seq.positions.back()[j];
-	auto xy = world2xy(camera, xyz);
-	return Viable{0, end(seq) - 1, end(seq), xyz, xyz, xy, xy};
-}
-
-void extend_forward(
-	std::vector<Sequence> const &, unsigned seqIndex,
-	Viable &, PlayerJoint, Camera const &, ViablesForJoint &);
-void extend_backward(
-	std::vector<Sequence> const &, unsigned seqIndex,
-	Viable &, PlayerJoint, Camera const &, ViablesForJoint &);
-
-void extend_from(
-	std::vector<Sequence> const & sequences, Position const & p,
-	PlayerJoint j,
-	Camera const & camera, ViablesForJoint & vfj)
-{
-	for (unsigned seq = 0; seq != sequences.size(); ++seq)
-	{
-		if (vfj.viables.find(seq) != vfj.viables.end()) continue;
-
-		auto & s = sequences[seq];
-
-		if (p == s.positions.front())
-			extend_forward(
-				sequences, seq,
-				vfj.viables[seq] = viableFront(s, j, camera),
-				j, camera, vfj);
-		else if (p == s.positions.back())
-			extend_backward(
-				sequences, seq,
-				vfj.viables[seq] = viableBack(s, j, camera),
-				j, camera, vfj);
-	}
-}
-
-void extend_forward(
-	std::vector<Sequence> const & sequences, unsigned const seqIndex,
-	Viable & via, PlayerJoint j, Camera const & camera, ViablesForJoint & vfj)
-{
-	auto & sequence = sequences[seqIndex];
-
-	for (; via.end != sequence.positions.size(); ++via.end)
-	{
-		V3 const v = sequence.positions[via.end][j];
-		V2 const xy = world2xy(camera, v);
-
-		if (distanceSquared(v, via.endV3) < 0.003) break;
-
-		double const
-			xydistSqrd = distanceSquared(xy, via.endxy),
-			xydist = std::sqrt(xydistSqrd);
-
-		if (xydist < 0.0005) break;
-
-		/*
-		auto const segment = LineSegment(xy, via.endxy);
-		for (auto && old : vfj.segments)
-			if (lineSegmentsIntersect(segment, old))
-				return via;
-		vfj.segments.push_back(segment);
-		*/
-
-		via.dist += xydist;
-		vfj.total_dist += xydist;
-		via.endV3 = v;
-		via.endxy = xy;
-	}
-
-	if (via.end == sequence.positions.size())
-		extend_from(sequences, sequence.positions.back(), j, camera, vfj);
-}
-
-void extend_backward(
-	std::vector<Sequence> const & sequences, unsigned const seqIndex,
-	Viable & via, PlayerJoint j, Camera const & camera, ViablesForJoint & vfj)
-{
-	auto & sequence = sequences[seqIndex];
-
-	int pos = via.begin;
-	--pos;
-	for (; pos != -1; --pos)
-	{
-		V3 const v = sequence.positions[pos][j];
-		V2 const xy = world2xy(camera, v);
-
-		if (distanceSquared(v, via.beginV3) < 0.003) break;
-
-		double const
-			xydistSqrd = distanceSquared(xy, via.beginxy),
-			xydist = std::sqrt(xydistSqrd);
-
-		if (xydistSqrd < 0.0005) break;
-
-		/*
-		auto const segment = std::make_pair(xy, via.beginxy);
-		for (auto && old : vfj.segments)
-			if (lineSegmentsIntersect(old, segment))
-				return via;
-		vfj.segments.push_back(segment);
-		*/
-
-		via.dist += xydist;
-		vfj.total_dist += xydist;
-		via.beginV3 = v;
-		via.beginxy = xy;
-	}
-
-	via.begin = pos + 1;
-
-	if (via.begin == 0)
-		extend_from(sequences, sequence.positions.front(), j, camera, vfj);
-}
-
-ViablesForJoint determineViables
-	( std::vector<Sequence> const & sequences, PlayerJoint const j
-	, unsigned const seq, unsigned const pos
-	, bool edit_mode, Camera const & camera)
-{
-	Sequence const & s = sequences[seq];
-
-	assert(pos != s.positions.size());
-
-	auto const jp = sequences[seq].positions[pos][j];
-	auto jpxy = world2xy(camera, jp);
-
-	ViablesForJoint r{0, {}};
-
-	if (!edit_mode && !jointDefs[j.joint].draggable) return r;
-
-	auto & v = r.viables[seq] = Viable{0, pos, pos+1, jp, jp, jpxy, jpxy};
-	extend_forward(sequences, seq, v, j, camera, r);
-	extend_backward(sequences, seq, v, j, camera, r);
-
-	if (r.total_dist < 0.3) return {0, {}};
-
-	return r;
-}
 
 void grid()
 {
