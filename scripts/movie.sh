543d68be0b6a5540499d2d87be9cec6d0299378a
Eelis van der Weegen <eelis@eelis.net>
2016-06-01 20:25:14 +0200

    movie.sh: Better params.

--- scripts/movie.sh
+++ scripts/movie.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ev
 rm -rf vidframes && mkdir vidframes
-src/grapplemap-mkvid --frames-per-pos 9 --start "neutral standing" --length 380 --dimensions 864x486
-avconv -framerate 50 -i 'vidframes/frame%05d.png' -vcodec libx264 -pix_fmt yuv420p  random.mp4
+src/grapplemap-mkvid --frames-per-pos 12 --start "symmetric staggered standing" --length 400 --dimensions 1280x720
+avconv -framerate 60 -i 'vidframes/frame%05d.png' -vcodec libx264 -pix_fmt yuv420p  random.mp4
