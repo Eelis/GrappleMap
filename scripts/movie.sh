61c0786eff2e021e730789db3d62ddd7a3b9414c
Eelis van der Weegen <eelis@eelis.net>
2016-11-21 00:14:17 +0100

    scripts/movie: Add ffmpeg command and update settings.

--- scripts/movie.sh
+++ scripts/movie.sh
@@ -2,5 +2,6 @@
 set -ev
 (cd src && scons -Q grapplemap-mkvid)
 rm -rf vidframes && mkdir vidframes
-src/grapplemap-mkvid --frames-per-pos 12 --length 415 --dimensions 1280x720
-avconv -framerate 60 -i 'vidframes/frame%05d.png' -vcodec libx264 -pix_fmt yuv420p  random.mp4
+src/grapplemap-mkvid --length 600
+avconv -framerate 60 -i 'vidframes/frame%05d.png' -vcodec libx264 -pix_fmt yuv420p random.mp4
+# ffmpeg -framerate 60 -i 'vidframes/frame%05d.png' -c:v libx264 -r 60 -pix_fmt yuv420p random.mp4
