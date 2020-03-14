67da2e5fa8882acea841727280f8379b90000635
Eelis van der Weegen <eelis@eelis.net>
2020-03-14 07:46:36 +0100

    Add script to install dev tools and libs on Ubuntu.

---
+++ scripts/apt-install-devtools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get install --no-install-recommends -yqq \
+  g++ \
+  scons \
+  pkg-config \
+  libgraphviz-dev \
+  libboost-dev \
+  libboost-regex-dev \
+  libboost-program-options-dev \
+  libftgl-dev \
+  libxine2-dev \
+  libglfw3-dev
